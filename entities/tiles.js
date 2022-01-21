ef5e166e921d72938fb7737de0d5e66e047beee0
Alex_mac <huy.trinh43@icloud.com>
2022-01-21 14:07:31 -0800

    draw tiles

--- entities/tiles.js
+++ entities/tiles.js
@@ -2,6 +2,8 @@ class Ground {
   constructor(game, x, y, w, h, type) {
     Object.assign(this, {game, x, y, w, h, type})
 
+    this.ground = ASSET_MANAGER.getAsset("./sprites/tiles/ground.png");
+    this.size = 50;
   }
 
 
@@ -9,7 +11,12 @@ class Ground {
 
   }
 
-  draw(ctx) {
-
-  }
+  draw(ctx){
+    for (let x1 = this.x; x1 < this.w; x1+=this.size)
+    {
+        for (let y1 = this.y; y1 < this.h; y1+=this.size){
+            ctx.drawImage(this.ground, 0, 0, 150, 150, x1, y1, this.size, this.size);
+        }
+    }
+}
 }
--- scenemanager.js
+++ scenemanager.js
@@ -1,5 +1,5 @@
 class SceneManager {
-    constructor(game) {
+    constructor(game, ) {
         this.game = game;
         this.game.camera = this;
         this.x = 0;
@@ -8,7 +8,7 @@ class SceneManager {
         // this.coins = 0;
 
         this.player = new Player(this.game, 100, 100);
-
+        this.ground = new Ground(this.game,0,0, PARAMS.CANVAS_WIDTH, PARAMS.CANVAS_HEIGHT, "");
         this.loadLevel(level1, true);
 
     }
@@ -16,6 +16,7 @@ class SceneManager {
     loadLevel(level, title) {
         this.x = 0;
         this.game.addEntity(this.player);
+        this.game.addEntity(this.ground);
     }
 
     update() {
