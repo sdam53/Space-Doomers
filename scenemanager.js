f7befa99c25b9a02658c26b4a9969246f214aa50
Shilnara Dam <ShilnaraWDam@gmail.com>
2022-01-23 01:12:05 -0800

    added flying monster to scene

--- scenemanager.js
+++ scenemanager.js
@@ -6,9 +6,10 @@ class SceneManager {
 
         // this.score = 0;
         // this.coins = 0;
-
-        this.player = new Player(this.game, 100, 100);
         this.ground = new Ground(this.game,0,0, PARAMS.CANVAS_WIDTH, PARAMS.CANVAS_HEIGHT, "");
+        this.player = new Player(this.game, 100, 100);
+        this.monster = new FlyingMonster(this.game, 1100, 300)
+
         // this.ground = new Ground(this.game,0,0, 1400, 800, "");
 
         this.loadLevel(level1, true);
@@ -17,8 +18,12 @@ class SceneManager {
 
     loadLevel(level, title) {
         this.x = 0;
+
+        this.game.addEntity(this.monster);
         this.game.addEntity(this.player);
         this.game.addEntity(this.ground);
+
+
     }
 
     update() {
