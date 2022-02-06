2fd6f72d74a29f087c453eb2a975a11e528ef011
Shilnara <shilnarawdam@gmail.com>
2022-02-06 03:55:22 -0800

    fixed BB

--- entities/door.js
+++ entities/door.js
@@ -10,7 +10,7 @@ class Door {
   }
 
   updateBB() {
-    this.BB = new BoundingBox(this.x + this.game.camera.x, this.y + this.game.camera.y, this.w, this.h);
+    this.BB = new BoundingBox(this.x, this.y, this.w, this.h);
     this.leftBB = new BoundingBox(this.x, this.y + 10, 3, this.h - 20);
     this.rightBB = new BoundingBox(this.x + this.w - 3, this.y + 10, 3, this.h - 20); 
     this.topBB = new BoundingBox(this.x + 10, this.y, this.w - 20, 3);
@@ -20,13 +20,14 @@ class Door {
   update() {
       this.x += this.game.camera.x;
       this.y += this.game.camera.y;
+      this.updateBB();
   }
 
   draw(ctx) {
     ctx.drawImage(this.sprites["unlocked down"], this.x, this.y, this.w, this.h);
     if (PARAMS.DEBUG && (typeof this.BB != 'undefined')) {
       ctx.strokeStyle = 'Brown';
-       ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
+      ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
       //ctx.strokeRect(this.leftBB.x, this.leftBB.y, this.leftBB.width, this.leftBB.height);
       //ctx.strokeRect(this.rightBB.x, this.rightBB.y, this.rightBB.width, this.rightBB.height);
       //ctx.strokeRect(this.topBB.x, this.topBB.y, this.topBB.width, this.topBB.height);
