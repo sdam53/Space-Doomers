b708ab334d81b15530e66575d2433a16005c2f08
Raz Consta <66283215+RazConsta@users.noreply.github.com>
2022-02-08 00:04:49 -0800

    Completed door except final door

--- entities/player.js
+++ entities/player.js
@@ -196,16 +196,44 @@ class Player {
 					}
 				}
 			}});
-			this.game.entities.powerup.forEach(function (entity) {
-				if (entity.BB && that.BB.collide(entity.BB)) {
-					if (entity instanceof Gear) {
-						ASSET_MANAGER.playAsset("./music/gear sound.wav");
-						entity.removeFromWorld = true;
-						that.gears++;
+
+			this.game.entities.portalw.forEach(function (entity) {
+				if (entity.BB && that.feetBB.collide(entity.BB)) {
+					if (entity instanceof Door) {
+						if (entity.leftBB && that.feetBB.collide(entity.leftBB)) // collides with left side of wall
+						{
+							that.x = that.x - RUN * TICK;
+							that.mapX -= RUN * TICK;
+						}
+						if (entity.rightBB && that.feetBB.collide(entity.rightBB)) // collides with right side of wall
+						{
+							that.x = that.x + RUN * TICK;
+							that.mapX += RUN * TICK;
+						}
+						if (entity.topBB && that.feetBB.collide(entity.topBB)) // collides with top side of wall
+						{
+							that.y = that.y - RUN * TICK;
+							that.mapY -= RUN * TICK;
+						}
+						if (entity.bottomBB && that.feetBB.collide(entity.bottomBB)) // collides with bottom side of wall
+						{
+							that.y = that.y + RUN * TICK;
+							that.mapY += RUN * TICK;
+						}
 					}
+				}});
+
+		this.game.entities.powerup.forEach(function (entity) {
+			if (entity.BB && that.BB.collide(entity.BB)) {
+				if (entity instanceof Gear) {
+					ASSET_MANAGER.playAsset("./music/gear sound.wav");
+					entity.removeFromWorld = true;
+					that.gears++;
 				}
-			});
-			this.updateBB();
+			}
+		});
+
+		this.updateBB();
 		}
 		
 		draw(ctx) {
